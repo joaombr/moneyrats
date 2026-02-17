083ab3aab772aa3d2788650e49ef803a64d13844
joaombr <joaomarcossalesbr@gmail.com>
2026-02-17 15:44:28 -0300

    Fix: Adicionando get_db no database.py

--- database.py
+++ database.py
@@ -13,3 +13,10 @@ if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
